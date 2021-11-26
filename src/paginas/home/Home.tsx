4c3b8cc20c6db83b9140a27bbcdf8aaf9573a512
valterchess <valterrekabano7@gmail.com>
2021-11-26 17:34:50 -0300

    aplicando o component home | basico

---
+++ src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+	return (
+		<>
+		<h1 className = "titulo" >Home</h1>
+		<img src="https://i.imgur.com/t2aqyYA.jpg" className = "img" alt="tabuleiro de xadrez"/>
+		</>
+	);
+}
+
+export default Home;
