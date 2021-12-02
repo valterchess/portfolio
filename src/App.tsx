e9196fe69d9242e42916fe9037892c09e8964b7c
valterchess <valterrekabano7@gmail.com>
2021-12-01 23:43:28 -0300

    Adicionando outros componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,32 @@
 import React from 'react';
-import NavBar from './components/statics/navbar/NavBar';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/statics/navbar/NavBar';
 import Footer from './components/statics/footer/Footer';
-import './App.css';
 import Home from './paginas/home/Home';
+import Login from './paginas/login/Login';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <NavBar></NavBar>
-      <Home></Home>
-      <Footer></Footer>
-		</>
+    <Router>
+      <Navbar />
+        <Switch>
+          <div style={{minHeight:'100vh'}}>
+
+          <Route exact path='/'>
+              <Login />
+            </Route>
+            <Route path='/login'>
+              <Login />
+            </Route>
+
+            <Route path='/home'>
+              <Home />
+            </Route>
+          </div>
+        </Switch>
+      <Footer />
+    </Router>
   );
 }
 
